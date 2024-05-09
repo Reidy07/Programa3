d6fcf059e7c6bb1e3e3a2e6cd516fe4e3bd1416d
Reidy07 <reidybrown1234@gmail.com>
2024-05-09 09:52:59 -0400

    Agregar archivos de proyecto.

---
+++ Programa3/Programa#3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Programa3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double numero3;
+            double numero4;
+            double suma;
+            double producto;
+
+            try
+            {
+                Console.WriteLine("Ingresar el primer numero:");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar el sugundo numero:");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(" INgresar el tercer numero:");
+                numero3 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresar el cuarto numero :");
+                numero4 = Convert.ToDouble(Console.ReadLine());
+
+                suma = numero1 + numero2 + numero3 + numero4;
+
+                producto = suma / 4;
+
+                Console.WriteLine(" La suma de todos los numero es " + suma);
+                Console.WriteLine("El producto de todos los numero es " + producto);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"El error de encuatra aqui {ex.Message}");
+            }
+        }
+    }
+}
